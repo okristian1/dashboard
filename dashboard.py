437eb8db408bbf1f7ce7333996e8fd72e5e608bd
skramole <okristian1@gmail.com>
2023-03-28 23:47:42 +0200

    added scheduler

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,5 @@
+#!/bin/env python
+
 import requests
 import sys
 import os
@@ -5,10 +7,18 @@ import subprocess
 import time 
 import json
 import requests
+import schedule
 
 from config import quote_api_key, news_api_key
 
+host_status=""
+quote=""
+news=""
+
+print("bagofdicks")
+
 def newsFeed():
+    global news
     limit = 7
     api_url = 'https://newsapi.org/v2/everything?domains=techcrunch.com'
     response = requests.get(api_url, headers={'X-Api-Key': news_api_key})
@@ -22,15 +32,13 @@ def newsFeed():
         if index == limit:
             break
     print ("========================================================================================================================")
-   # data = output['articles'][0]['title']
-   # print (data)
-    #news = "Todays Headlines: " + response.json#[0]["title"]
 
 
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKCYAN = '\033[96m'
+
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
@@ -40,6 +48,7 @@ class bcolors:
 
 
 def getQuote():
+    global quote
     limit = 1
     api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(limit)
     response = requests.get(api_url, headers={'X-Api-Key': quote_api_key})
@@ -57,44 +66,40 @@ def getQuote():
         print ("************************************************************************************************************************")
 
 def getHostStatus():
-    while True:
-        time.sleep(5)
-        with open(os.devnull, 'w') as DEVNULL:
-            try:
-                subprocess.check_call(
-                    ['ping', '-c', '3', 'plexmediaserver'],
-                    stdout=DEVNULL,  # suppress output
-                    stderr=DEVNULL
-                )
-                sys.stdout.write('\r'+"                                                  ")
-                is_up = True
-                host_status = (f"Plex Media Server Status: {bcolors.OKGREEN}available{bcolors.ENDC}")
-                sys.stdout.write('\r'+host_status)
-                sys.stdout.flush()
-            except subprocess.CalledProcessError:
-                is_up = False
-                host_status = (f"Plex Media Server Status: {bcolors.WARNING}not available{bcolors.ENDC}")
-                sys.stdout.write('\r'+host_status)
-                sys.stdout.flush()
+    global host_status
+    with open(os.devnull, 'w') as DEVNULL:
+        try:
+            subprocess.check_call(
+                ['ping', '-c', '3', '192.168.68.111'],
+                stdout=DEVNULL,  # suppress output
+                stderr=DEVNULL
+            )
+            sys.stdout.write('\r'+"                                                  ")
+            is_up = True
+            host_status = (f"Plex Media Server Status: {bcolors.OKGREEN}available{bcolors.ENDC}")
+            sys.stdout.write('\r'+host_status)
+            sys.stdout.flush()
+        except subprocess.CalledProcessError:
+            is_up = False
+            host_status = (f"Plex Media Server Status: {bcolors.WARNING}not available{bcolors.ENDC}")
+            sys.stdout.write('\r'+host_status)
+            sys.stdout.flush()
 
-def bufferWriter(derp):
-    with open("/dev/tty1", "wb+", buffering=0) as term:
-        if derp == "clear":
-            term.write("\033[2J".encode())
-            term.write("\033[H".encode())
-        elif derp == "space":
-            print (" ")
-            #term.write("\n\n\n".encode())
-        #term.write("Terminal Write Access Granted".encode())
-#        while True:
-#            print(term.read(1).decode(), end='')
-#            sys.stdout.flush()
+def clear():
+    os.system('clear')
+
+
+clear()
+newsFeed()
+getQuote()
+getHostStatus()
+
+schedule.every(1130).seconds.do(clear)
+schedule.every(1130).seconds.do(newsFeed)
+schedule.every(1130).seconds.do(getQuote)
+schedule.every(5).seconds.do(getHostStatus)
 
 while True:
-    bufferWriter("clear")
-    newsFeed()
-    getQuote()
-    getHostStatus()
-    time.sleep(3600)
+    schedule.run_pending()
+    time.sleep(5)
 
-#sys.stdout.write("\r" + time.ctime())
